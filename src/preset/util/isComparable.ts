806b72c6c26ecce0ae41898c46a73f400838525b
Kozo Oeda <kozo.oeda@gmail.com>
2024-04-17 21:49:14 +0900

    Add: array types

--- src/preset/util/isComparable.ts
+++ src/preset/util/isComparable.ts
@@ -15,12 +15,19 @@ const isBoolean = (symbol: DeterministicSymbol | NonDeterministicSymbol) : boole
   return symbols.includes(symbol);
 };
 
+const isArray = (symbol: DeterministicSymbol | NonDeterministicSymbol) : boolean => {
+  const symbols: Array<DeterministicSymbol | NonDeterministicSymbol> = ["array", "random-array"];
+  return symbols.includes(symbol);
+};
+
 export const isComparable = (a: AllValues, b: AllValues): boolean => {
   if (isString(a.symbol) && isString(b.symbol)) {
     return true;
   } else if (isNumber(a.symbol) && isNumber(b.symbol)) {
     return true;
   } else if (isBoolean(a.symbol) && isBoolean(b.symbol)) {
+    return true;
+  } else if (isArray(a.symbol) && isArray(b.symbol)) {
     return true;
   } else {
     return false;
--- src/value.ts
+++ src/value.ts
@@ -1,8 +1,8 @@
 import strEnum from "./util/strEnum";
 import { TOM } from "./util/tom";
 
-const _dS = strEnum(["number", "string", "boolean"]);
-const _nonDS = strEnum(["random-number", "random-string", "random-boolean"]);
+const _dS = strEnum(["number", "string", "boolean", "array"]);
+const _nonDS = strEnum(["random-number", "random-string", "random-boolean", "random-array"]);
 
 export const deterministicSymbols = TOM.keys(_dS);
 export const nonDeterministicSymbols = TOM.keys(_nonDS);
@@ -18,17 +18,20 @@ interface Value<T, name extends DeterministicSymbol | NonDeterministicSymbol> {
 export type FixedNumberValue = Value<number, "number">
 export type FixedStringValue = Value<string, "string">
 export type FixedBooleanValue = Value<boolean, "boolean">
+export type FixedArrayValue = Value<AllValues[], "array">
 
 export type RandomNumberValue = Value<number, "random-number">
 export type RandomStringValue = Value<string, "random-string">
 export type RandomBooleanValue = Value<boolean, "random-boolean">
+export type RandomArrayValue = Value<AllValues[], "random-array">
 
 export type NumberValue = FixedNumberValue | RandomNumberValue
 export type StringValue = FixedStringValue | RandomStringValue
 export type BooleanValue = FixedBooleanValue | RandomBooleanValue
+export type ArrayValue = FixedArrayValue | RandomArrayValue
 
-export type DeterministicValues = FixedNumberValue | FixedStringValue | FixedBooleanValue
-export type NonDeterministicValues = RandomNumberValue | RandomStringValue | RandomBooleanValue
+export type DeterministicValues = FixedNumberValue | FixedStringValue | FixedBooleanValue | FixedArrayValue
+export type NonDeterministicValues = RandomNumberValue | RandomStringValue | RandomBooleanValue | RandomArrayValue
 export type AllValues = DeterministicValues | NonDeterministicValues
 
 
@@ -37,7 +40,7 @@ export function isFixedNumber(val: AllValues): val is FixedNumberValue {
 }
 
 export function isRandomNumber(val: AllValues): val is RandomNumberValue {
-  return val.symbol=== "random-number";
+  return val.symbol === "random-number";
 }
 
 export function isFixedString(val: AllValues): val is FixedStringValue {
@@ -54,5 +57,13 @@ export function isFixedBoolean(val: AllValues): val is FixedBooleanValue {
 
 export function isRandomBoolean(val: AllValues): val is RandomBooleanValue {
   return val.symbol === "random-boolean";
+}
+
+export function isFixedArray(val: AllValues): val is FixedArrayValue {
+  return val.symbol === "array";
+}
+
+export function isRandomArray(val: AllValues): val is RandomArrayValue {
+  return val.symbol === "random-array";
 }
 
