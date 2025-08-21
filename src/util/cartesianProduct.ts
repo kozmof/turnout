ad5b9185c76ad3e2d058fff4688dc0b7b0aafb3c
Kozo Oeda <kozo.oeda@gmail.com>
2025-08-21 20:54:10 +0900

    Add type restrictions

--- src/util/cartesianProduct.ts
+++ src/util/cartesianProduct.ts
@@ -1,7 +1,7 @@
-export const cartesianProduct = <T>(...arrays: T[][]): T[][] => {
-  return arrays.reduce<T[][]>(
-    (acc, curr) =>
-      acc.flatMap((a) => curr.map((b) => [...a, b])),
-    [[]]
+export const cartesianProduct = <T, U, V>(
+  ...arrays: [T[], U[], V[]]
+): Array<[T, U, V]> => {
+  return arrays[0].flatMap((t) =>
+    arrays[1].flatMap((u) => arrays[2].map((v) => [t, u, v] as [T, U, V]))
   );
 };
