ef183f83ef8561ba76f57a9acce9889c4c8aa5cc
Kozo Oeda <kozo.oeda@gmail.com>
2025-10-23 21:28:58 +0900

    cleanup

--- src/hank/hank.ts
+++
@@ -1,9 +0,0 @@
-import { type KnotId } from '../knot/knot';
-import { type Brand } from '../util/brand';
-
-export type HankId = Brand<string, 'hank'>
-
-export interface Hank {
-  id: HankId
-  knotIds: KnotId[]
-}
