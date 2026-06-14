cca7adf48d631cfd53e499bea03f021eede23ccd
Kozo Oeda <kozo.oeda@gmail.com>
2026-06-14 04:33:07 +0000

    improved

---
+++ scripts/check-proto.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+
+sh scripts/generate-proto.sh
+
+if [ -x /usr/bin/git ]; then
+  git_bin=/usr/bin/git
+else
+  git_bin=git
+fi
+
+git_home="$PWD/.git-home"
+mkdir -p "$git_home"
+HOME="$git_home" XDG_CONFIG_HOME="$git_home/.config" "$git_bin" diff --exit-code -- \
+  packages/go/converter/internal/emit/turnoutpb \
+  packages/ts/scene-runner/src/types
